f91b4c3cd8d4094de617d513e835b170841c1409
mrshappy0 <51244673+mrshappy0@users.noreply.github.com>
2022-11-16 17:31:38 -0700

    angular show goerli last block and click event binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
-
+import { ethers } from 'ethers';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'example-angular-frontend';
+  lastBlockNumber: number | undefined;
+
+  counter = 0;
+
+  constructor() {
+    ethers.providers
+      .getDefaultProvider('goerli')
+      .getBlock('latest')
+      .then((block) => {
+        this.lastBlockNumber = block.number;
+      });
+  }
+
+  clickMe() {
+    this.counter++
+  }
 }
